704689c1c5f30010c59bdeadf8e86193cffa5fea
Guilherme Illescas <gui.illescas@gmail.com>
2021-06-10 13:48:09 -0300

    Add components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
 
-import { StylesContainer, Title } from './styles'
+import { HighLightCard } from '../../components/HighlightCard';
+
+import {
+  StylesContainer,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighLightCards,
+} from './styles'
 
 export function Dashboard() {
   return (
     <StylesContainer>
-      <Title>Oi</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/59893752?v=4'
+              }}
+            />
+
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Guilherme</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+    
+      <HighLightCards>
+        <HighLightCard
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="up"
+        />
+        <HighLightCard
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 13 de abril"
+          type="down"
+        />
+        <HighLightCard
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="total"
+        />
+      </HighLightCards>
     </StylesContainer>
   );
 };
