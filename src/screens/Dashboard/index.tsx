26e3bc056c0df23f3893d5233c049362a2e85f0a
Guilherme Illescas <gui.illescas@gmail.com>
2021-06-10 01:02:09 -0300

    Create dashboard page

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { StylesContainer, Title } from './styles'
+
+export function Dashboard() {
+  return (
+    <StylesContainer>
+      <Title>Oi</Title>
+    </StylesContainer>
+  );
+};
