efe7eac0bb026be4f76dc7f9098982185ae17965
Guilherme Illescas <gui.illescas@gmail.com>
2021-06-11 14:04:26 -0300

    Add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
-import { Dashboard } from './src/screens/Dashboard';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+
+import Register from './src/screens/Register';
 
 import theme from './src/global/styles/theme';
 
-export default function App() {
+export default function App(): ReactElement {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
