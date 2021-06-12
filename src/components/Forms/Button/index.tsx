bf09a24081e71a06a6bda5043effc4024b2329e1
Guilherme Illescas <gui.illescas@gmail.com>
2021-06-12 00:19:55 -0300

    Change button type

--- src/components/Forms/Button/index.tsx
+++ src/components/Forms/Button/index.tsx
@@ -1,16 +1,20 @@
 import React, { ReactElement } from 'react';
-
-import { TouchableOpacityProps } from 'react-native';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
 import { Container, Title } from './styles';
 
-interface IButtonProps extends TouchableOpacityProps {
+interface IButtonProps extends RectButtonProps {
   title: string;
+  onPress: () => void;
 }
 
-export function Button({ title, ...rest }: IButtonProps): ReactElement {
+export function Button({
+  title,
+  onPress,
+  ...rest
+}: IButtonProps): ReactElement {
   return (
-    <Container activeOpacity={0.7} {...rest}>
+    <Container onPress={onPress} activeOpacity={0.7} {...rest}>
       <Title>{title}</Title>
     </Container>
   );
